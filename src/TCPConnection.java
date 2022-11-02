efa22846a30a7221fcadfa07ae32efd3a2dca266
ArtemKoroliuk <96040624+ArtemKoroliuk@users.noreply.github.com>
2022-11-02 17:52:56 +0200

    chatstartWithTCPConnection

--- src/TCPConnection.java
+++ src/TCPConnection.java
@@ -1,7 +1,59 @@
+package src;
 import java.io.*;
-import  java.net.socket;
-public  class TCPConnection {
+import java.net.Socket;
+
+public class TCPConnection {
     private final Socket socket;
-    private final Thread rxThread;
-    private  final Buff
+    private  Thread rxThread;
+    private final TCPConnectionListener eventListener;
+    private final BufferedReader in;
+    private final BufferedWriter out;
+    public TCPConnection(TCPConnectionListener eventListener,String ipAddres, int port) throws IOException {
+        this(eventListener,new Socket(ipAddres,port));
+    }
+
+    public TCPConnection(TCPConnectionListener eventListener,Socket socket)
+            throws IOException {
+        this.eventListener = eventListener;
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        rxThread=new Thread(() -> {
+            try {
+                eventListener.onConnectionReady(TCPConnection.this);
+                while (rxThread.isInterrupted()){
+                    String msg=in.readLine();
+                    eventListener.onReceiveString(TCPConnection.this,msg);
+                }
+            } catch (IOException e) {
+                eventListener.onException(TCPConnection.this,e);
+            }
+            finally {
+                eventListener.onDisconnect(TCPConnection.this);
+            }
+        });
+        rxThread.start();
+    }
+    public synchronized void sendString(String value){
+        try {
+            out.write(value + "\r\n");
+            out.flush();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this,e);
+            disconnect();
+        }
+
+    }
+    public  synchronized void disconnect() {
+        rxThread.interrupt();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            eventListener.onException(TCPConnection.this,e);
+        }
+    }
+    @Override
+    public  String toString(){
+        return "TcpConnection :"+ socket.getInetAddress()+":" + socket.getPort();
+    }
 }
